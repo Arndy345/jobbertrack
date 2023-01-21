2190f4a6c589ec16f80e898a33e24b84afd72b9e
Nonso Andrew <nonsoandrew1@gmail.com>
2023-01-21 15:56:06 +0100

    installed packages

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const app = express();
 const User = require("./models/user");
-const Job = require("./models/jobs");
-const Router = require("./routes/User");
+const userRoute = require("./routes/User");
+const jobsRoute = require("./routes/Job");
+require("express-async-errors");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+const auth = require("./middleware/authentication");
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
-const jwt = require("jsonwebtoken");
+
+app.set("trust proxy", 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // limit each IP to 100 requests per windowMs
+	})
+);
+
 app.use(express.json());
-
-app.use("/", Router);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use("/api/v1/auth", userRoute);
+app.use("/api/v1", auth, jobsRoute);
 app.get("/", async (req, res) => {
 	const user = await User.find({});
 
 	res.status(200).json(user);
 });
-app
-app.get("/jobs", async (req, res) => {
-	const jobs = await Job.find({});
-
-	res.status(200).json(jobs);
-});
-
-app.post("/jobs", async (req, res) => {
-	const body = req.body;
-
-	const jobs = await Job.create(body);
-
-	res.status(201).json(jobs);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
