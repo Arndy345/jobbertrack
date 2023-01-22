385d109c3dfff79c513de8bdddc467c6a0176487
Nonso Andrew <nonsoandrew1@gmail.com>
2023-01-22 15:58:49 +0100

    FIX: models and routes

--- models/jobs.js
+++ models/jobs.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const jobSchema = new Schema({
-	company: { type: String, required: true },
-	position: { type: String, required: true },
-	status: {
-		type: String,
-		required: true,
-		enum: ["pending", "interview", "declined"],
-		default: "pending",
+const jobSchema = new Schema(
+	{
+		company: { type: String, required: true },
+		position: { type: String, required: true },
+		status: {
+			type: String,
+			required: true,
+			enum: ["pending", "interview", "declined"],
+			default: "pending",
+		},
+		createdBy: {
+			type: mongoose.Types.ObjectId,
+			ref: "User",
+			required: [true, "Please provide user"],
+		},
 	},
-});
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("Job", jobSchema);
