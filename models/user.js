710940bc57f41a496f5615b24a6bc8e5fcb31fa6
Nonso Andrew <nonsoandrew1@gmail.com>
2023-01-30 16:06:55 +0100

    Fix: user email unique

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userSchema = new Schema({
 	name: {
 		type: String,
@@ -10,12 +11,18 @@ const userSchema = new Schema({
 	email: {
 		type: String,
 		required: [true, "Provide email"],
+		// This value can either be `true` to use the default error
+		// message or a non-empty string to use a custom one.
 		unique: true,
+		match: [
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			"Please provide a valid email",
+		],
 	},
 	password: {
 		type: String,
 		required: [true, "Provide password"],
-		min: [6, "Password too short"],
+		minlength: [6, "Password too short"],
 	},
 });
 
@@ -40,6 +47,15 @@ userSchema.methods.comparePasswords =
 			this.password
 		);
 	};
+userSchema.methods.createToken = function () {
+	return jwt.sign(
+		{ email: this.email, userId: this._id },
+		process.env.JWT_SECRET,
+		{
+			expiresIn: process.env.JWT_LIFETIME,
+		}
+	);
+};
 
 module.exports = mongoose.model(
 	"User",
